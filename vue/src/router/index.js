f3c45d754c5301f8501b47c74054ef52a9895ffa
roydonGuo <3133010060@qq.com>
2022-09-15 23:14:39 +0800

    路由js

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,14 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/Manage.vue'
+import store from "@/store"
 
 Vue.use(VueRouter)
 
 const routes = [
     {
         path: '/',
-        name: 'Manage',
-        component: HomeView
+        component: () => import('../views/Manage.vue'),
+        redirect: '/home',
+        children: [
+            {path: 'home', name: '主页', component: () => import('../views/Home.vue')},
+            {path: 'user', name: '用户管理', component: () => import('../views/User.vue')},
+        ]
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/Login.vue'),
+
+    },
+    {
+        path: '/register',
+        name: 'register',
+        component: () => import('../views/Register.vue'),
+
     },
 
 ]
@@ -17,6 +33,13 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes
+})
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+    localStorage.setItem("currentPathName", to.name)  // 设置当前的路由名称，为了在Header组件中去使用
+    store.commit("setPath")  // 触发store的数据更新
+    next()  // 放行路由
 })
 
 export default router
