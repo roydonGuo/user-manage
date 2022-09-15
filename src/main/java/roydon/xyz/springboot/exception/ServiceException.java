c8938dc20cfa699036d1cdee734399b212f6caaf
roydonGuo <3133010060@qq.com>
2022-09-15 23:06:03 +0800

    ServiceException 自定义异常

---
+++ src/main/java/roydon/xyz/springboot/exception/ServiceException.java
@@ -0,0 +1,19 @@
+package roydon.xyz.springboot.exception;
+
+import lombok.Getter;
+
+/**
+ * 自定义异常
+ */
+@Getter
+public class ServiceException extends RuntimeException {
+
+    private String code;
+
+    public ServiceException(String code, String msg) {
+        super(msg);
+        this.code = code;
+    }
+
+}
+
