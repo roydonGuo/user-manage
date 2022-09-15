c8f5ff45197484b8518091c4a6983de8961c2d89
roydonGuo <3133010060@qq.com>
2022-09-15 22:51:08 +0800

    UserController

--- src/main/java/roydon/xyz/springboot/controller/UserController.java
+++ src/main/java/roydon/xyz/springboot/controller/UserController.java
@@ -1,64 +1,85 @@
 package roydon.xyz.springboot.controller;
 
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.poi.excel.ExcelReader;
+import cn.hutool.poi.excel.ExcelUtil;
+import cn.hutool.poi.excel.ExcelWriter;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.logging.log4j.util.Strings;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import roydon.xyz.springboot.common.Constants;
 import roydon.xyz.springboot.common.Result;
+import roydon.xyz.springboot.controller.dto.UserDTO;
 import roydon.xyz.springboot.entity.User;
-import roydon.xyz.springboot.service.UserService;
+import roydon.xyz.springboot.service.IUserService;
 
+import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
- * Created by Intellij IDEA
- * Author: yi cheng
- * Date: 2022/9/13
- * Time: 18:48
- **/
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author roydon
+ * @since 2022-09-15
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    @Resource
+    private IUserService userService;
 
-    @GetMapping
-    public List<User> index() {
-        List<User> userList = userService.list();
-        return userList;
+    @PostMapping("/login")
+    public Result login(@RequestBody UserDTO userDTO) {
+        if (StrUtil.isBlank(userDTO.getUsername()) || StrUtil.isBlank(userDTO.getPassword())) {
+            return Result.error(Constants.CODE_400, "参数错误");
+        }
+        return Result.success(userService.login(userDTO));
     }
 
-    /**
-     * 分页查询
-     * /user/page?pageNum=1&pageSize=10，limit第一个参数 = (pageNum - 1) * pageSize
-     *
-     * @param pageNum
-     * @param pageSize
-     * @param username
-     * @param email
-     * @param address
-     * @return
-     */
-//    @GetMapping("/page")
-//    public Map<String, Object> findPage(@RequestParam Integer pageNum,
-//                                        @RequestParam Integer pageSize,
-//                                        @RequestParam String username,
-//                                        @RequestParam String email,
-//                                        @RequestParam String address) {
-//        pageNum = (pageNum - 1) * pageSize;
-//        username = "%" + username + "%";
-//        email = "%" + email + "%";
-//        address = "%" + address + "%";
-//        List<User> data = userMapper.selectPage(pageNum, pageSize, username,email,address);
-//        Integer total = userMapper.selectTotal(username,email,address);
-//        Map<String, Object> res = new HashMap<>();
-//        res.put("data", data);
-//        res.put("total", total);
-//        return res;
-//    }
+    @PostMapping("/register")
+    public Result register(@RequestBody UserDTO userDTO) {
+
+        return Result.success(userService.register(userDTO));
+    }
+
+    // 新增或者更新
+    @PostMapping
+    public boolean save(@RequestBody User user) {
+        return userService.saveOrUpdate(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean delete(@PathVariable Integer id) {
+        return userService.removeById(id);
+    }
+
+    @DeleteMapping("/del/batch")
+    public boolean deleteBatch(@RequestBody List<Integer> ids) {
+        return userService.removeByIds(ids);
+    }
+
+    @GetMapping
+    public List<User> findAll() {
+        return userService.list();
+    }
+
+    @GetMapping("/{id}")
+    public User findOne(@PathVariable Integer id) {
+        return userService.getById(id);
+    }
+
     @GetMapping("/page")
     public IPage<User> findPage(@RequestParam Integer pageNum,
                                 @RequestParam Integer pageSize,
@@ -72,19 +93,78 @@ public class UserController {
         return userService.page(new Page<>(pageNum, pageSize), queryWrapper);
     }
 
-    @PostMapping
-    public Result save(@RequestBody User user) {
+    /**
+     * 导出接口
+     * 从浏览器导出(推荐)，可以自定义路径
+     *
+     * @param response
+     * @throws Exception
+     */
+    @GetMapping("/export")
+    public void export(HttpServletResponse response) throws Exception {
+        // 从数据库查询出所有的数据
+        List<User> list = userService.list();
+        // 通过工具类创建writer 写出到磁盘路径
+//        ExcelWriter writer = ExcelUtil.getWriter(filesUploadPath + "/用户信息.xlsx");
+        // 1.在内存操作，写出到浏览器
+        ExcelWriter writer = ExcelUtil.getWriter(true);
+        // 2.自定义标题别名
+//        writer.addHeaderAlias("id", "id");
+        writer.addHeaderAlias("username", "用户名");
+        writer.addHeaderAlias("password", "密码");
+        writer.addHeaderAlias("nickname", "昵称");
+        writer.addHeaderAlias("email", "邮箱");
+        writer.addHeaderAlias("phone", "电话");
+        writer.addHeaderAlias("address", "地址");
+        writer.addHeaderAlias("createTime", "创建时间");
+        writer.addHeaderAlias("avatarUrl", "头像");
 
-        return Result.success(userService.saveOrUpdate(user));
+        // 3.一次性写出list内的对象到excel，使用默认样式，强制输出标题
+        writer.write(list, true);
+
+        // 4.设置浏览器响应的格式
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8");
+        String fileName = URLEncoder.encode("用户信息", "UTF-8");
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName + ".xlsx");
+
+        ServletOutputStream out = response.getOutputStream();
+        writer.flush(out, true);
+        out.close();
+        writer.close();
     }
 
-    @DeleteMapping("/{id}")
-    public Result delete(@PathVariable Integer id) {
-        return Result.success(userService.removeById(id));
+    /**
+     * excel 导入
+     *
+     * @param file
+     * @throws Exception
+     */
+    @PostMapping("/import")
+    public Boolean imp(MultipartFile file) throws Exception {
+        InputStream inputStream = file.getInputStream();
+        ExcelReader reader = ExcelUtil.getReader(inputStream);
+        // 方式1：(推荐) 通过 javabean的方式读取Excel内的对象，但是要求表头必须是英文，跟javabean的属性要对应起来
+//        List<User> list = reader.readAll(User.class);
+
+        // 方式2：忽略表头的中文，直接读取表的内容
+        List<List<Object>> list = reader.read(1);
+        System.out.println("======" + list);
+        List<User> users = CollUtil.newArrayList();
+        for (List<Object> row : list) {
+            User user = new User();
+            // row.get(0) 得到的是id，不需要id，因为数据库id为自增
+            user.setUsername(row.get(1).toString());
+            user.setPassword(row.get(2).toString());
+            user.setNickname(row.get(3).toString());
+            user.setEmail(row.get(4).toString());
+            user.setPhone(row.get(5).toString());
+            user.setAddress(row.get(6).toString());
+            user.setAvatarUrl(row.get(8).toString());
+            users.add(user);
+        }
+
+        userService.saveBatch(users);
+        return true;
     }
 
-    @DeleteMapping("/del/batch")
-    public Result deleteBatch(@RequestBody List<Integer> ids) {
-        return Result.success(userService.removeByIds(ids));
-    }
 }
