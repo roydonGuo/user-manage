83757c3eb8562b5bcb58cf39e31c47f1cc33ce36
roydonGuo <3133010060@qq.com>
2022-09-15 22:52:12 +0800

    user实体类

--- src/main/java/roydon/xyz/springboot/entity/User.java
+++ src/main/java/roydon/xyz/springboot/entity/User.java
@@ -1,31 +1,77 @@
 package roydon.xyz.springboot.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
- * Created by Intellij IDEA
- * Author: yi cheng
- * Date: 2022/9/13
- * Time: 18:39
- **/
-@Data
-@TableName(value = "sys_user")
-public class User {
-    @TableId(type = IdType.AUTO)
+ * <p>
+ *  代码生成器生成实体类
+ * </p>
+ *
+ * @author roydon
+ * @since 2022-09-15
+ */
+@Getter
+@Setter
+@ToString
+@TableName("sys_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
+
+    /**
+     * 用户名
+     */
     private String username;
-    @JsonIgnore
-    private String password; //JsonIgnore 忽略此字段不展示给前端
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 昵称
+     */
     private String nickname;
+
+    /**
+     * 邮箱
+     */
     private String email;
+
+    /**
+     * 电话
+     */
     private String phone;
+
+    /**
+     * 地址
+     */
     private String address;
-    @TableField(value = "avatar_url")  // 指定数据库的字段名称
-    private String avatar;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 头像
+     */
+    private String avatarUrl;
+
+
 
 }
