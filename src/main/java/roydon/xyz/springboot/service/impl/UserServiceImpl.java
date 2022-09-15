e6bdab2c51c755a786b764605525e91b2464c31f
roydonGuo <3133010060@qq.com>
2022-09-15 23:12:27 +0800

    UserServiceImpl

---
+++ src/main/java/roydon/xyz/springboot/service/impl/UserServiceImpl.java
@@ -0,0 +1,65 @@
+package roydon.xyz.springboot.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.log.Log;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import roydon.xyz.springboot.common.Constants;
+import roydon.xyz.springboot.controller.dto.UserDTO;
+import roydon.xyz.springboot.entity.User;
+import roydon.xyz.springboot.exception.ServiceException;
+import roydon.xyz.springboot.mapper.UserMapper;
+import roydon.xyz.springboot.service.IUserService;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author roydon
+ * @since 2022-09-15
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+
+    private static final Log LOG = Log.get();
+
+    @Override
+    public UserDTO login(UserDTO userDTO) {
+        User one = getUserInfo(userDTO);
+        if (one != null) {
+            BeanUtil.copyProperties(one, userDTO, true);
+            return userDTO;
+        } else {
+            throw new ServiceException(Constants.CODE_600, "用户名或密码错误");
+        }
+    }
+
+    @Override
+    public User register(UserDTO userDTO) {
+        User one = getUserInfo(userDTO);
+        if (one == null) {
+            one = new User();
+            BeanUtil.copyProperties(userDTO, one, true);
+            save(one);  // 把 copy完之后的用户对象存储到数据库
+        } else {
+            throw new ServiceException(Constants.CODE_600, "用户已存在");
+        }
+        return one;
+    }
+
+    private User getUserInfo(UserDTO userDTO) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("username", userDTO.getUsername());
+        queryWrapper.eq("password", userDTO.getPassword());
+        User one;
+        try {
+            one = getOne(queryWrapper); // 从数据库查询用户信息
+        } catch (Exception e) {
+            LOG.error(e);
+            throw new ServiceException(Constants.CODE_500, "系统错误");
+        }
+        return one;
+    }
+}
