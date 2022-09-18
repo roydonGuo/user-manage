94cf803dd4aedcd834659b790adf1728ba025932
roydonGuo <3133010060@qq.com>
2022-09-18 13:03:17 +0800

    mapper

---
+++ src/main/java/roydon/xyz/springboot/mapper/FileMapper.java
@@ -0,0 +1,13 @@
+package roydon.xyz.springboot.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import roydon.xyz.springboot.entity.Files;
+
+/**
+ * Created by Intellij IDEA
+ * Author: yi cheng
+ * Date: 2022/9/17
+ * Time: 21:14
+ **/
+public interface FileMapper extends BaseMapper<Files> {
+}
