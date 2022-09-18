d2382e866bc396a965f20841249dea1e25a55174
roydonGuo <3133010060@qq.com>
2022-09-18 16:25:52 +0800

    request.js

--- vue/src/utils/request.js
+++ vue/src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import ElementUI from "element-ui";
 
 const request = axios.create({
     baseURL: 'http://localhost:9090',
@@ -11,7 +12,10 @@ const request = axios.create({
 request.interceptors.request.use(config => {
     config.headers['Content-Type'] = 'application/json;charset=utf-8';
 
-    // config.headers['token'] = user.token;  // 设置请求头
+    let user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null
+    if (user) {
+        config.headers['token'] = user.token;  // 设置请求头
+    }
     return config
 }, error => {
     return Promise.reject(error)
@@ -29,6 +33,13 @@ request.interceptors.response.use(
         // 兼容服务端返回的字符串数据
         if (typeof res === 'string') {
             res = res ? JSON.parse(res) : res
+        }
+        // 权限验证不通过给出提示
+        if(res.code==='401'){
+            ElementUI.Message({
+                message: res.msg,
+                type: 'error'
+            });
         }
         return res;
     },
