e37bce9c5f37ea64fcaa32e964892c14fceae819
roydonGuo <3133010060@qq.com>
2022-09-18 16:31:46 +0800

    主页

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
 <div>
-  <h1>主页</h1>
+  <el-calendar v-model="value">
+  </el-calendar>
 </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      value: new Date()
+    }
+  }
 }
 </script>
 
