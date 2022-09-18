82022d4cd7bbf436d7f14f7ca6157b95e3eeff29
roydonGuo <3133010060@qq.com>
2022-09-18 13:01:31 +0800

    拦截器

---
+++ src/main/java/roydon/xyz/springboot/config/InterceptorConfig.java
@@ -0,0 +1,29 @@
+package roydon.xyz.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roydon.xyz.springboot.config.interceptor.JwtInterceptor;
+
+/**
+ * Created by Intellij IDEA
+ * Author: yi cheng
+ * Date: 2022/9/17
+ * Time: 19:44
+ **/
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Bean
+    public JwtInterceptor jwtInterceptor() {
+        return new JwtInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login", "/user/register", "/**/export", "/**/import","/file/**");
+    }
+}
