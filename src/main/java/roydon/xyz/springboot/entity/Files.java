a1ff71c56e439e61e273ea2c3322e49b266527b3
roydonGuo <3133010060@qq.com>
2022-09-18 13:04:15 +0800

    Files

---
+++ src/main/java/roydon/xyz/springboot/entity/Files.java
@@ -0,0 +1,28 @@
+package roydon.xyz.springboot.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+/**
+ * Created by Intellij IDEA
+ * Author: yi cheng
+ * Date: 2022/9/17
+ * Time: 21:15
+ **/
+@Data
+@TableName("sys_file")
+public class Files {
+
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private String name;
+    private String type;
+    private Long size;
+    private String url;
+    private String md5;
+    private Boolean isDelete;
+    private Boolean enable;
+
+}
