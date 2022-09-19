8680faf7484cfe21e609f3f32ea4c13e3373bb42
roydonGuo <3133010060@qq.com>
2022-09-19 23:52:02 +0800

    角色权限枚举

---
+++ src/main/java/roydon/xyz/springboot/common/RoleEnum.java
@@ -0,0 +1,11 @@
+package roydon.xyz.springboot.common;
+
+/**
+ * Created by Intellij IDEA
+ * Author: yi cheng
+ * Date: 2022/9/19
+ * Time: 21:25
+ **/
+public enum RoleEnum {
+    ROLE_ADMIN,ROLE_USER
+}
