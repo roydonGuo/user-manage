61f28e1070c4fee6391803a7018d7ab597abb69e
roydonGuo <3133010060@qq.com>
2022-09-19 23:56:49 +0800

    菜单业务层

---
+++ src/main/java/roydon/xyz/springboot/service/IMenuService.java
@@ -0,0 +1,16 @@
+package roydon.xyz.springboot.service;
+
+import roydon.xyz.springboot.entity.Menu;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author roydon
+ * @since 2022-09-19
+ */
+public interface IMenuService extends IService<Menu> {
+
+}
