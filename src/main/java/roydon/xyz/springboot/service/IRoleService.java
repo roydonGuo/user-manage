51352936c9f62995155b6dea71fb5d7d699f850c
roydonGuo <3133010060@qq.com>
2022-09-19 23:57:34 +0800

    用户身份业务层

---
+++ src/main/java/roydon/xyz/springboot/service/IRoleService.java
@@ -0,0 +1,21 @@
+package roydon.xyz.springboot.service;
+
+import roydon.xyz.springboot.entity.Role;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author roydon
+ * @since 2022-09-18
+ */
+public interface IRoleService extends IService<Role> {
+
+    void setRoleMenu(Integer roleId, List<Integer> menuIds);
+
+    List<Integer> getRoleMenu(Integer roleId);
+}
