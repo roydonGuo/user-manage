006f11c029c522866f791ed24277726cf29231f8
roydonGuo <3133010060@qq.com>
2022-09-19 23:55:05 +0800

    权限

---
+++ src/main/java/roydon/xyz/springboot/entity/Role.java
@@ -0,0 +1,49 @@
+package roydon.xyz.springboot.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author roydon
+ * @since 2022-09-18
+ */
+@Getter
+@Setter
+@TableName("sys_role")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * id
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 唯一标识
+     */
+    private String flag;
+
+
+
+}
