38fccea1d0be37ec5ad7df294f88fc0b0a173d47
roydonGuo <3133010060@qq.com>
2022-09-20 23:42:20 +0800

    组件

--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -11,13 +11,16 @@
       </div>
 
     </div>
-    <el-dropdown style="width: 100px; cursor: pointer">
+    <el-dropdown style="width: 160px; cursor: pointer;text-align: right">
       <div style="display: inline-block">
         <img :src="user.avatarUrl" alt=""
              style="width: 30px; border-radius: 50%; position: relative; top: 10px; right: 5px">
         <span>{{ user.nickname }}</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
       </div>
       <el-dropdown-menu slot="dropdown" style="width: 100px; text-align: center">
+        <el-dropdown-item style="font-size: 14px; padding: 5px 0">
+          <router-link to="/password">修改密码</router-link>
+        </el-dropdown-item>
         <el-dropdown-item style="font-size: 14px; padding: 5px 0">
           <router-link to="/person">个人信息</router-link>
         </el-dropdown-item>
@@ -35,17 +38,17 @@ export default {
   props: {
     collapseBtnClass: String,
     collapse: Function,
-    pathName: String
+    pathName: String,
+    user: Object
   },
   data() {
     return {
-      user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
+      // user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : {}
     }
   },
   methods: {
     logout() {
-      this.$router.push("/login")
-      localStorage.removeItem("user")
+      this.$store.commit("logout")
       this.$message.success("退出成功")
     }
   },
