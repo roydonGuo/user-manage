bad287805352ad2917e9dfb162a435ec15b0c502
roydonGuo <3133010060@qq.com>
2022-09-20 23:43:21 +0800

    store

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from "@/router"
 
 Vue.use(Vuex)
 
@@ -10,6 +11,11 @@ const store = new Vuex.Store({
     mutations: {
         setPath (state) {
             state.currentPathName = localStorage.getItem("currentPathName")
+        },
+        logout(){
+            localStorage.removeItem("user")
+            localStorage.removeItem("menus")
+            router.push("/login")
         }
     }
 })
