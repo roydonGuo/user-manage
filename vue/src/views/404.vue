7f32d9677a4c86f3f9b76bd27c21ebf61577fdf6
roydonGuo <3133010060@qq.com>
2022-09-20 23:44:20 +0800

    404##

---
+++ vue/src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="height: 100vh;overflow: hidden">
+    <img src="../assets/404.png" style="width: 100%;height: 100%;">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
